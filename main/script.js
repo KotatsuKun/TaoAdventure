9830914ff0cfcf7eb6626422deb77093383bd2ac
KotatsuKun <131630713+KotatsuKun@users.noreply.github.com>
2024-11-18 22:52:49 -0300

    adding and changing somethings

---
+++ main/script.js
@@ -0,0 +1,12 @@
+const passnext = document.querySelectorAll('.next-btn');
+
+passnext.forEach(button => {
+    button.addEventListener('click', function(){
+        const active = document.querySelector('.active');
+        const nextpass = 'pass-' + this.getAttribute('data-pass');
+
+        active.classList.remove('active');
+        document.getElementById(nextpass).classList.add('active');
+
+    })
+})
