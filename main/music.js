a2f6962f0219b761dce2342de7c490f742e42b4b
KotatsuKun <131630713+KotatsuKun@users.noreply.github.com>
2024-11-20 21:41:25 -0300

    changing more stuff

--- main/music.js
+++ main/music.js
@@ -1,12 +1,21 @@
 
+var music1 = new Audio("Tian Guan Ci Fu OST&BGM 17 桃源.mp3");
+var music2 = new Audio("The Legend of Ashitaka.mp3");
 
-function play(){
+function Music1play(){
 
-    var music1 = new Audio("Tian Guan Ci Fu OST&BGM 17 桃源.mp3");
     music1.play();
     music1.volume = 0.6;
     music1.loop = true;
 
 }
 
+function toMusic2(){
+
+    music1.pause()
+    music2.play();
+    music2.volume = 0.6;
+    music2.loop = true;
+
+}
 
