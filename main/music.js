4ef722d232e45770ec077966f6dd204eaa44d8f1
KotatsuKun <131630713+KotatsuKun@users.noreply.github.com>
2024-11-24 18:58:42 -0300

    adding and changing stuff

--- main/music.js
+++ main/music.js
@@ -1,6 +1,13 @@
 
 var music1 = new Audio("Tian Guan Ci Fu OST&BGM 17 桃源.mp3");
-var music2 = new Audio("The Legend of Ashitaka.mp3");
+var music2 = new Audio("入云间 - 《天官赐福》动画配乐.mp3");
+var music3 = new Audio("心魔 - 《天官赐福》动画配乐.mp3");
+var music4 = new Audio("飞升 - 《天官赐福》动画配乐.mp3");
+var music5 = new Audio("与君山 - 《天官赐福》动画配乐.mp3")
+
+
+
+
 
 function Music1play(){
 
@@ -12,10 +19,53 @@ function Music1play(){
 
 function toMusic2(){
 
-    music1.pause()
+    music1.pause();
     music2.play();
     music2.volume = 0.6;
     music2.loop = true;
 
 }
 
+function toMusic3(){
+
+    music4.pause();
+    music3.pause();
+    music2.pause();
+    music5.pause();
+    music1.pause();
+    music3.play();
+    music3.volume = 0.6;
+    music3.loop = true;
+
+}
+
+function toMusic4(){
+
+    music5.pause();
+    music2.pause();
+    music3.pause();
+    music4.play();
+    music4.volume = 0.6;
+    music4.loop = true;
+
+}
+
+function toMusic5(){
+
+    music3.pause();
+    music2.pause();
+    music1.pause();
+    music4.pause();
+    music5.play();
+    music5.volume = 0.6;
+    music5.loop = true;
+
+}
+
+function stop(){
+    music1.pause();
+    music2.pause();
+    music3.pause();
+    music4.pause();
+    music5.pause();
+}
