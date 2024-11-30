6007de902236efc89ce91bbc0bf1d18fe3908858
KotatsuKun <131630713+KotatsuKun@users.noreply.github.com>
2024-11-29 23:36:43 -0300

    changing and refactoring some logic

--- main/script.js
+++ main/script.js
@@ -11,14 +11,15 @@ passnext.forEach(button => {
     })
 })
 
+
 function background1(){
-    document.body.style.background = "url('https://static.wikia.nocookie.net/modao-zushi/images/5/59/Lotus_Pier.png/revision/latest?cb=20191123060530'), background-size: cover, background-repeat: no-repeat, background-position: fill;";
+    document.body.style.backgroundImage = "url('https://static.wikia.nocookie.net/modao-zushi/images/5/59/Lotus_Pier.png/revision/latest?cb=20191123060530')";
 }
 
 function background2(){
-    document.body.style.background = "url('https://i.pinimg.com/736x/39/25/0c/39250c63d80f67c24606bb5f80ffd3c8.jpg')";
+    document.body.style.backgroundImage = "url('https://i.pinimg.com/736x/39/25/0c/39250c63d80f67c24606bb5f80ffd3c8.jpg')";
 }
 
 function background3(){
-    document.body.style.background = "url('https://static.wikia.nocookie.net/modao-zushi/images/1/1e/Burial_Mounds_-_2.png/revision/latest?cb=20200512073257')";
+    document.body.style.backgroundImage = "url('https://static.wikia.nocookie.net/modao-zushi/images/1/1e/Burial_Mounds_-_2.png/revision/latest?cb=20200512073257')";
 }
