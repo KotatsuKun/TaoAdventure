267e5d01ca0c14123071ebe8a6d7ce5d52ddb117
KotatsuKun <131630713+KotatsuKun@users.noreply.github.com>
2024-11-30 15:58:23 -0300

    refactoring code

--- main/music.js
+++ main/music.js
@@ -5,59 +5,51 @@ var music3 = new Audio("心魔 - 《天官赐福》动画配乐.mp3");
 var music4 = new Audio("飞升 - 《天官赐福》动画配乐.mp3");
 var music5 = new Audio("与君山 - 《天官赐福》动画配乐.mp3")
 
-
+var volume = 0.4
 
 
 
 function Music1play(){
 
+    stop()
     music1.play();
-    music1.volume = 0.6;
+    music1.volume = volume;
     music1.loop = true;
 
 }
 
 function toMusic2(){
 
-    music1.pause();
+    stop()
     music2.play();
-    music2.volume = 0.6;
+    music2.volume = volume;
     music2.loop = true;
 
 }
 
 function toMusic3(){
 
-    music4.pause();
-    music3.pause();
-    music2.pause();
-    music5.pause();
-    music1.pause();
+    stop()
     music3.play();
-    music3.volume = 0.6;
+    music3.volume = volume;
     music3.loop = true;
 
 }
 
 function toMusic4(){
 
-    music5.pause();
-    music2.pause();
-    music3.pause();
+    stop()
     music4.play();
-    music4.volume = 0.6;
+    music4.volume = volume;
     music4.loop = true;
 
 }
 
 function toMusic5(){
 
-    music3.pause();
-    music2.pause();
-    music1.pause();
-    music4.pause();
+    stop()
     music5.play();
-    music5.volume = 0.6;
+    music5.volume = volume;
     music5.loop = true;
 
 }
