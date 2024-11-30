fead0318e5eb4f24072ddf27b5ccabec097c2129
KotatsuKun <131630713+KotatsuKun@users.noreply.github.com>
2024-11-30 15:38:18 -0300

    adding and changing somethings

--- main/script.js
+++ main/script.js
@@ -23,3 +23,12 @@ function background2(){
 function background3(){
     document.body.style.backgroundImage = "url('https://static.wikia.nocookie.net/modao-zushi/images/1/1e/Burial_Mounds_-_2.png/revision/latest?cb=20200512073257')";
 }
+
+function background4(){
+    document.body.style.backgroundImage = "url('https://static.wikia.nocookie.net/heaven-officials-blessing/images/2/20/Puji_shrine_donghua.png/revision/latest?cb=20201121072112')";
+}
+
+
+function background5(){
+    document.body.style.backgroundImage = "url('https://images6.alphacoders.com/136/thumb-350-1367773.webp')";
+}
